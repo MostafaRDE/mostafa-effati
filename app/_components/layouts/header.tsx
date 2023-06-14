bee51755cd21b40ae17d97b6657670ee91b777fa
Mostafa Rostam Dokht Effati <mostafa.rostamdokht@gmail.com>
2023-06-14 15:52:36 +0330

    Add first header config

--- app/_components/layouts/header.tsx
+++ app/_components/layouts/header.tsx
@@ -1,6 +1,28 @@
+import { menuHeaderItems } from '@/modules/menu'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Header()
 {
     return (
-        <header></header>
+        <header>
+            <div>
+                <Image src={ require('@/assets/images/logo-outline.svg') } alt="" width={ 44 } />
+            </div>
+
+            <div>
+                <nav>
+                    <ul>
+                        {
+                            Object.entries(menuHeaderItems).map(([ k, v ]) =>(
+                                <li>
+                                    <Link href={ v.href || '#' }>{ v.translateKey }</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 }
---
+++ modules/menu.ts
@@ -0,0 +1,14 @@
+export type Menu = Record<string, MenuItemInterface>
+
+export interface MenuItemInterface
+{
+    translateKey: string
+    href?: string
+}
+
+export const menuHeaderItems: Menu = {
+    about: { translateKey: 'about', href: '#about' },
+    whatIDo: { translateKey: 'what_i_do', href: '#what-i-do' },
+    portfolio: { translateKey: 'portfolio', href: '#portfolio' },
+    letsTalk: { translateKey: 'lets_talk', href: '#lets-talk' },
+}

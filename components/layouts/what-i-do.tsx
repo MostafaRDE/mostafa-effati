e1e8d954cac5ef50c3bfe9928d4b0791850ff56c
Mostafa Rostam Dokht Effati <mostafa.rostamdokht@gmail.com>
2023-06-17 11:43:53 +0330

    Add structure of what-i-do

--- components/layouts/what-i-do.tsx
+++ components/layouts/what-i-do.tsx
@@ -1,10 +1,45 @@
 import { Params } from '@/modules/interfaces/props.interface'
 
+const data: {
+    imageSrc?: string,
+    text: string,
+    boldText: string,
+}[] = [
+    {
+        text: 'Do you need to redesign a site or improve the design of your site or app to improve the experience of your customers?',
+        boldText: 'I can do it.',
+    },
+    {
+        text: 'Do you need to redesign a site or improve the design of your site or app to improve the experience of your customers?',
+        boldText: 'I can do it.',
+    },
+    {
+        text: 'Do you need to redesign a site or improve the design of your site or app to improve the experience of your customers?',
+        boldText: 'I can do it.',
+    },
+]
+
 export default function whatIDo({ lang, t }: Params)
 {
     return (
-        <div>
+        <section id="what-i-do">
             <h2>{ t[ 'glossaries' ][ 'what_i_do_2' ] }</h2>
-        </div>
+            
+            <div className="columns-1 md:columns-3 md:gap-x-8 lg:gap-x-16 xl:gap-x-32">
+
+                {
+                    data.map(item =>
+                    {
+                        return (
+                            <div className="mb-16 last:mb-0 md:mb-0">
+                                <p>{ item.text }</p>
+                                <mark className="block mt-4 bg-transparent font-bold">{ item.boldText }</mark>
+                            </div>
+                        )
+                    })
+                }
+                
+            </div>
+        </section>
     )
 }

8affc265c4339a3e9f99c39b57228994fe6bdcbb
Mostafa Rostam Dokht Effati <mostafa.rostamdokht@gmail.com>
2023-07-28 17:03:34 +0330

    fix: Fixing in load default locale

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -1,6 +1,6 @@
 import 'server-only'
 import '@/dictionary'
-import type { Locale } from './i18n-config'
+import { i18n, type Locale } from './i18n-config'
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
@@ -8,4 +8,5 @@ const dictionaries = {
     'en-us': () => import('./_dictionary/en-us.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]()
+console.log(dictionaries)
+export const getDictionary = async (locale: Locale) => dictionaries[locale || i18n.defaultLocale]()
--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    images: {
+        dangerouslyAllowSVG: true,
+        formats: ['image/webp'],
+    },
     trailingSlash: true,
 }
 
